8c0a9c62d289ba285301974fc8060ecc7690f00b
Jason Campbell <jason@borderstylo.com>
2011-04-27 15:06:14 -0700

    First

---
+++ drag-and-drop.js
@@ -0,0 +1,43 @@
+var dropHandler = {
+  handleEvent: function(event){
+    if (event.type === 'dragenter') { this.dragenter(event); }
+    if (event.type === 'dragexit') { this.dragexit(event); }
+    if (event.type === 'dragover') { this.dragover(event); }
+    if (event.type === 'drop') { this.drop(event); }
+  },
+
+  dragenter: function(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.debug('dragenter')
+  },
+
+  dragexit: function(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.debug('dragexit')
+  },
+
+  dragover: function(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.debug('dragover')
+  },
+
+  drop: function(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.debug('drop')
+  },
+};
+
+var dropbox = document.getElementById("dropbox")
+
+dropbox.addEventListener("dragenter", dropHandler, false);
+dropbox.addEventListener("dragexit", dropHandler, false);
+dropbox.addEventListener("dragover", dropHandler, false);
+dropbox.addEventListener("drop", dropHandler, false);
